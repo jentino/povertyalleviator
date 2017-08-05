dce64453401e2e9452d52c21cc0423d73fb19a87
jentino <jentino@gmail.com>
2017-08-06 01:11:56 +0200

    added dot5

--- includes/dot_dbupdate.php
+++ includes/dot_dbupdate.php
@@ -19,6 +19,7 @@ $dot1 = $_GET['dot1'];
 $dot2 = $_GET['dot2'];
 $dot3 = $_GET['dot3'];
 $dot4 = $_GET['dot4'];
+$dot5 = $_GET['dot5'];
 
 //$sql = "INSERT INTO tbl_dots (dot_dir) VALUES ('$dot_name') WHERE dot_id='1';
 
@@ -26,7 +27,7 @@ $dot4 = $_GET['dot4'];
 
 //$sql = "INSERT INTO tbl_dots (dot_dir) VALUES ('$dot_name') WHERE dot_id='1';
 
-$sql = "UPDATE tblsignals SET candle1='$candle1', candle5='$candle5', candle6='$candle6', dot1='$dot1', dot2='$dot2', dot3='$dot3', dot4='$dot4' WHERE candle_id=1";
+$sql = "UPDATE tblsignals SET candle1='$candle1', candle5='$candle5', candle6='$candle6', dot1='$dot1', dot2='$dot2', dot3='$dot3', dot4='$dot4', dot5='$dot5' WHERE candle_id=1";
 
 if ($conn->query($sql) === TRUE) {
     echo "sql executed successfully.<br>";

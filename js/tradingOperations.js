64a62a826fde6a582093f416b227c8e251970dba
jentino <jentino@gmail.com>
2017-08-08 02:16:54 +0200

    fixed return value
    but not colourizing

--- js/tradingOperations.js
+++ js/tradingOperations.js
@@ -1,21 +1,28 @@
+
+
 //////////////////////////////////////////////////////////////////  COLOUR WIN AND LOSS
 function checkWinOrLoss(amount){
-
+	document.getElementById("showtype").innerHTML = typeof amount;
 	if (amount > 0) {
 		playSoundCustom(20);
 		countwins++;
-		return amount.bold().fontcolor("Green");
+		re = 0;
+		amount.bold().fontcolor("Green");
 	}
-	else if(amount < 0){
+	else{
 		playSoundCustom(22);
 		countlosses++;
-		return amount.bold().fontcolor("Red");
+		re++;
+		amount.bold().fontcolor("Red");
 	}
+
+	return amount;
 }
 
 var tcount = -4;
 //////////////////////////////////////////////////////////////////  AUTO TRADINNG
 function calcTradeTime(newTime) {
+	showRe();
 	var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
 	d.setUTCSeconds(newTime);
 	dm = d.getMinutes();
@@ -24,19 +31,19 @@ function calcTradeTime(newTime) {
 		if(tcount!=dm){
 			//alert(DOT3 + ":" + Sig);
 			if(DOT3 == "red" && Sig == "red") {
-				setColor(Sellit,tradeamount[0],9);
+				setColor(Sellit,tradeamount[re],9);
 				tcount = dm;
 				document.getElementById("tradeaction").innerHTML = "SELL";
 			} else if (DOT3 == "red" && Sig == "green") {
-				setColor(Buyit,tradeamount[0],1);
+				setColor(Buyit,tradeamount[re],1);
 				tcount = dm;
 				document.getElementById("tradeaction").innerHTML = "BUY";
 			} else if (DOT3 == "green" && Sig == "red") {
-				setColor(Buyit,tradeamount[0],1);
+				setColor(Buyit,tradeamount[re],1);
 				tcount = dm;
 				document.getElementById("tradeaction").innerHTML = "BUY";
 			} else if (DOT3 == "green" && Sig == "green") {
-				setColor(Sellit,tradeamount[0],9);
+				setColor(Sellit,tradeamount[re],9);
 				tcount = dm;
 				document.getElementById("tradeaction").innerHTML = "BUY";
 			} 
